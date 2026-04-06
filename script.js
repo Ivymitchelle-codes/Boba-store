a09b10fe14fc8bac3414d7eeb1e9d3297b726d44
Ivy Mitchelle <ivymitchellewangari@gmail.com>
2026-04-06 06:47:50 +0300

    Added add to cart function and Price math

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ updateCarousel();
 
 // Auto rotate every 2.5 seconds
 setInterval(rotateCarousel, 2500);
+
+let cart = [];
+
+function addToCart(productName,price)
+{
+   //Add the item to our cart array
+   cart.push({name: productName, price:price}); 
+
+   //Calculate  the totalusing a loop (The math)
+   let total = 0;
+   cart.forEach(item=>{
+    total += item.price;
+   });
+
+   //Update the ui : show the count and the total money
+   document.getElementById('cart-count').innerText = cart.length;
+
+   //If you have a cart total span updat here
+
+   if(document.getElementById('cart-total')){
+    document.getElementById('cart-total').innerText = currentTotal;
+   }
+   
+   //update the ui(we'll alert for now, but see step 3)
+   //alert(`${productName} added! Total items:${cart.length} | Total : Ksh ${total}`);
+
+   console.log("New Total : Ksh", + currentTotal);
+}
